e144af325b48528959c1c031684afbadc6589fdb
Saw <sawsoemoenyunt@gmail.com>
2025-11-11 20:13:30 +1000

    add TruckSerice Class

---
+++ Services/TruckService.cs
@@ -0,0 +1,32 @@
+﻿using VolvoTruckManagement.Model;
+
+namespace VolvoTruckManagement.Services
+{
+    public class TruckService : ITruckService
+    {
+        public Task<ServiceResponse<Truck>> CreateTruck(Truck truck)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<bool>> DeleteTruck(int truckNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<Truck>> GetTruckAsync(int truckNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<Truck>>> GetTrucksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<Truck>> UpdateTruck(Truck product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
