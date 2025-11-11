94e0ff7f9a790dc793c9b2fef601cdda335ff442
Saw <sawsoemoenyunt@gmail.com>
2025-11-11 19:17:28 +1000

    Add Truck model with default-initialized properties

---
+++ Components/Model/Truck.cs
@@ -0,0 +1,11 @@
+﻿namespace VolvoTruckManagement.Components.Model
+{
+    public class Truck
+    {
+        public int TruckNumber { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string VIN { get; set; } = string.Empty;
+        public int BuildDate { get; set; } = 0;
+        public string Comment { get; set; } = string.Empty;
+    }
+}
