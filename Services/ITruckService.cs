815a4e4ced5987cd507d08cce251acb5bae7e473
Saw <sawsoemoenyunt@gmail.com>
2025-11-11 20:13:06 +1000

    add ITruckService Interface

---
+++ Services/ITruckService.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using VolvoTruckManagement.Model;
+
+namespace VolvoTruckManagement.Services
+{
+    public interface ITruckService
+    {
+        Task<ServiceResponse<List<Truck>>> GetTrucksAsync();
+        Task<ServiceResponse<Truck>> GetTruckAsync(int truckNumber);
+        Task<ServiceResponse<Truck>> CreateTruck(Truck truck);
+        Task<ServiceResponse<Truck>> UpdateTruck(Truck product);
+        Task<ServiceResponse<bool>> DeleteTruck(int truckNumber);
+    }
+}
